9b1b139ee42e7c1d31c82fe012406df9af69be76
rushia <jonasg.13452@gmail.com>
2022-09-13 22:10:03 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,29 @@
+from tkinter import *
+from tkinter import ttk
+
+class Root(Tk):
+    def __init__(self):
+        super(Root,self).__init__()
+        self.title("Python Tkinter")
+        self.geometry("600x600")
+        self.resizable(0, 0)
+
 def main():
-    pass
+    root = Root()
+    ttk.Button(root, text="Quit", command = root.destroy).place(x = 200, y = 200)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
+
+"""
+TODO
+make resizable, learn how to use grid/pack for buttons and such
+
+default screen
+options - create new folder, view quotes, view watchlists
+
+libraries - tkinter, market api, statplots, probably math
+
+need a database at some point
+"""
