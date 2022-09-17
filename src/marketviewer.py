1cb5d98968a44f8024e55ba279a1b9966ac62a78
rushia <jonasg.13452@gmail.com>
2022-09-17 14:37:52 -0400

    Update marketviewer.py

--- src/marketviewer.py
+++ src/marketviewer.py
@@ -1,11 +1,12 @@
 import os
+from tkinter import Y
 import requests
 import urllib.request
 
 # dunno if these are needed yet
 import pandas as pd
 import numpy as np
-from pandas_datareader import data as datareader
+from pandas_datareader import data as pdr
 import datetime as dt
 from datetime import date, timedelta
 
@@ -18,26 +19,28 @@ pio.templates
 import yfinance as yf
 
 def main():
+    yf.pdr_override()
+
     sym = input("Enter stock symbol: ")
 
     start = dt.datetime(2022, 1, 1)
     now = dt.datetime.now()
-    df = datareader.get_data_yahoo(sym, start, now)
+    df = pdr.get_data_yahoo(sym, start, now)
 
-    df.round(2)
+    df.round(3)
 
-    df = pd.DataFrame()
+    data = pd.DataFrame()
     sma10 ='sma10'
     sma80 ='sma80'
-    #df[sma10]= df.iloc[:,4].rolling(window=10).mean()
-    #df[sma80]= df.iloc[:,4].rolling(window=80).mean()
-    df.round(2)
+    data[sma10]= df.iloc[:,4].rolling(window=10).mean()
+    data[sma80]= df.iloc[:,4].rolling(window=80).mean()
+    data.round(2)
 
     fig = go.Figure(data=[go.Candlestick(x=df.index, name = 'Price',
                     open=df['Open'], high=df['High'], 
-                    low=df['Low'], close=df['Close']),])
-        #go.Scatter(x=data1.index, y=data1.sma10, name='SMA 10'),
-        #go.Scatter(x=data1.index, y=data1.sma80,name='SMA 80')])
+                    low=df['Low'], close=df['Close']),
+        go.Scatter(x=data.index, y=data.sma10, name='SMA 10'),
+        go.Scatter(x=data.index, y=data.sma80,name='SMA 80')])
 
     for template in ["plotly_dark"]:
         fig.layout.font.family = 'Balto'
