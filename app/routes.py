103cba42ea69d9048dca922062076321b27bc032
rushia <jonasg.13452@gmail.com>
2022-10-03 23:25:54 -0400

    folder setup

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user, AnonymousUserMixin
 import plotly
 import json
 
@@ -21,7 +22,7 @@ def view():
         fig = view_f(text)
         # endcode plot as json
         fjson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        return render_template("return.html", figJSON=fjson)
+        return render_template("view.html", figJSON=fjson)
     elif request.method == "GET":
         return redirect(url_for("routes.index"))
 
@@ -31,8 +32,8 @@ folders
 """
 @routes.route("/folders", methods=["POST", "GET"])
 def add_to_folder():
-    if request.method == "POST":
-        req = request.form["req"]
-        if req == "create":
-            pass
-    return render_template("folders.html")
+    user = current_user
+    if isinstance(user, AnonymousUserMixin) or isinstance(user, type(None)):
+        return redirect(url_for("auth.signup"))
+    else:
+        return render_template("folders.html")
